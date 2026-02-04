6b2d6a1165a0175b93888b59e16ae649efcb0bc0
Uqi12 <mynameissyauqi.12345@gmail.com>
2026-02-05 07:21:30 +0800

    syauqi

--- lib/providers/note_provider.dart
+++ lib/providers/note_provider.dart
@@ -1,5 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:notepad/models/note.dart';
 
 class NoteProvider extends ChangeNotifier {
-  
+  final List<Note> _notes = [];
+  List<Note> get getNotes{
+    return _notes;
+  }
+
+  void NotesOperation(){
+    addNewNote('First Note', 'First Note Description');
+  }
+
+  void addNewNote(String title, String description) {
+    Note note = Note(title, description);
+    _notes.add(note);
+    notifyListeners();
+  }
 }
